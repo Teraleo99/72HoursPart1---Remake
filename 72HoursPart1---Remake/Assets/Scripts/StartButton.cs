20e75c40a29f72fde87d243a9a48a1744b47cb37
Teraleo99 <43067374+Teraleo99@users.noreply.github.com>
2019-05-26 04:06:54 +0100

    Update 1
    Added timer that results in game over when it runs out

---
+++ 72HoursPart1---Remake/Assets/Scripts/StartButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Allows us to use scene loading function
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        //Reset the score
+        PlayerPrefs.DeleteKey("score");
+
+        //Load first level
+        SceneManager.LoadScene("Level1");
+    }
+
+}
---
+++ 72HoursPart1---Remake/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Using statement for unity UI code
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    public Text TimerText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        TimerText.text = currentTime.ToString("0"); 
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+            GameOver();
+        }
+        
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
